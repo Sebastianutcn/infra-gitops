cee69b97cd29f5bfa4891eac5068b2f9b0c1c139
Sebastianutcn <babiciusebastian@gmail.com>
2025-04-17 18:18:07 +0300

    commit

---
+++ main.tf
@@ -0,0 +1,26 @@
+provider "google" {
+  credentials = file("credentials.json")
+  project     = "gitops-457111"
+  region      = "europe-west1"
+  zone        = "europe-west1-b"
+
+}
+
+resource "google_compute_instance" "gitops_vm" {
+  name         = "vm-instance"
+  machine_type = "e2-medium"
+  zone         = "europe-west1-b"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {}
+  }
+  #   metadata_startup_script = file("startup-script.sh")
+
+}
