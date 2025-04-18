35016b67c8896a10caf4d63aef7d2537dd8def88
Sebastianutcn <babiciusebastian@gmail.com>
2025-04-18 11:46:37 +0300

    commit

---
+++ scripts/startup-script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+# Install Docker
+apt-get update
+apt-get install -y docker.io curl conntrack
+
+# Enable Docker
+systemctl enable docker
+systemctl start docker
+
+# Install Minikube
+curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-amd64
+install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
+
+# Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# Start Minikube
+minikube start --driver=docker --memory=4096 --cpus=2
+
+
