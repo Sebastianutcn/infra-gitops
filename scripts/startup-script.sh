96d5d73863dd0221452a476fe19d37989481ee14
Sebastianutcn <babiciusebastian@gmail.com>
2025-04-18 15:00:24 +0300

    commit

--- scripts/startup-script.sh
+++ scripts/startup-script.sh
@@ -9,6 +9,8 @@ apt-get install -y curl apt-transport-https ca-certificates gnupg lsb-release \
 systemctl enable docker
 systemctl start docker
 
+sleep 20
+
 # Install kubectl
 curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x kubectl
@@ -19,7 +21,7 @@ curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikub
 install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
 
 # Start Minikube with Docker driver
-minikube start --driver=docker --force
+minikube start --driver=docker
 
 # Wait for Minikube to be ready
 sleep 100
