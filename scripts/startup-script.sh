641928b9a32157708a212552a3960a25556911b3
Sebastianutcn <babiciusebastian@gmail.com>
2025-04-18 12:13:02 +0300

    commit

--- scripts/startup-script.sh
+++ scripts/startup-script.sh
@@ -13,12 +13,11 @@ systemctl enable docker
 curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-amd64
 install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
 
-# Install kubectl
-# curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-# chmod +x kubectl
-# mv kubectl /usr/local/bin/
-
 # Start Minikube
 minikube start --driver=docker
 
+# Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
 
