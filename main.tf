31ebc8e5a5f183db2e434e5db0c42e68bb27205b
sebastian <babiciusebastian@gmail.com>
2025-05-16 18:56:55 +0300

    New commit

--- main.tf
+++ main.tf
@@ -1,31 +1,23 @@
 provider "google" {
-  # credentials = file("credentials.json")
   project     = "gitops-457111"
   region      = "europe-west1"
   zone        = "europe-west1-b"
-
 }
 
-# resource "google_compute_instance" "gitops_vm" {
-#   name         = "vm-instance"
-#   machine_type = "e2-medium"
-#   zone         = "europe-west1-b"
-#   boot_disk {
-#     initialize_params {
-#       image = "debian-cloud/debian-11"
-#     }
-#   }
+resource "google_compute_instance" "gitops_vm" {
+  name         = "vm-instance"
+  machine_type = "e2-medium"
+  zone         = "europe-west1-b"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
 
-#   network_interface {
-#     network = "default"
-
-#     access_config {}
-#   }
-# }
-
-# create a bucket in gcp
 resource "google_storage_bucket" "gitops_bucket" {
   name     = "gitops-bucket-457111"
   location = "EU"
   force_destroy = true
 }
+
+
