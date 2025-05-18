1a21bdb36bb741f167bb639f08f51fcb19e24c77
sebastian <babiciusebastian@gmail.com>
2025-05-18 10:10:44 +0300

    New commit

--- main.tf
+++ main.tf
@@ -4,22 +4,22 @@ provider "google" {
   zone        = "europe-west1-b"
 }
 
-resource "google_compute_instance" "gitops_vm" {
-  name         = "vm-instance"
-  machine_type = "e2-medium"
-  zone         = "europe-west1-b"
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-  network_interface {
-    network = "default"
-    access_config {
-      // Ephemeral IP
-    }
-  }
-}
+# resource "google_compute_instance" "gitops_vm" {
+#   name         = "vm-instance"
+#   machine_type = "e2-medium"
+#   zone         = "europe-west1-b"
+#   boot_disk {
+#     initialize_params {
+#       image = "debian-cloud/debian-11"
+#     }
+#   }
+#   network_interface {
+#     network = "default"
+#     access_config {
+#       // Ephemeral IP
+#     }
+#   }
+# }
 
 resource "google_storage_bucket" "gitops_bucket" {
   name     = "gitops-bucket-457111"
